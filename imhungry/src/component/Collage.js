5e48e87d93c79e0f4fc184368f8d52d7019ef975
kbchandr <kbchandr@usc.edu>
2019-04-24 20:33:39 -0700

    heroku initial commit

---
+++ imhungry/src/component/Collage.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import './CSS/Collage.css';
+
+
+
+class Collage extends React.Component {
+
+    render(){
+        let images = this.props.images;
+
+        return(
+            <div className="gallery">
+                {images.map(function(url, index){
+                    return (
+                    <figure className={"gallery__item gallery__item--"+index} key={index}>
+                        <img src={url} className="gallery__img" alt={"Image "+index}/>
+                    </figure>
+                  );
+                })}
+            </div>
+
+        );
+    }
+
+}
+
+
+export default Collage;
